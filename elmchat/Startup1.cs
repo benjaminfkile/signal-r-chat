6470485ac7a8297ba91cc606bd0932db68d3674b
Ben Kile <ben.f.kile@elmutility.com>
2021-05-27 15:42:43 -0600

    first

---
+++ elmchat/Startup1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(elmchat.Startup))]
+namespace elmchat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
